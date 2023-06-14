7442ec85c896e3dc23183a30152f0b187bd8f805
ChaviSchatz <hand2b26@mbjcomp.org.il>
2023-06-14 21:52:36 +0300

    start

---
+++ repositories/repository.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+
+export class Repository{
+    constructor(model){
+        this.model = model;
+    }
+
+    async getAll(){
+        let items = await this.model.find({});
+        return items;
+    }
+
+    async getById(id){
+        let item = await this.model.findById(id);
+        return item;
+    }
+
+    async insert(data){
+        let result = await this.model.create(data);
+        return result;
+    }
+
+    async update(id, data){
+        let result = await this.model.findByIdAndUpdate(id, data, { new: true });
+        return result;
+        }    
+
+    async delete(id){
+        let result = await this.model.findByIdAndDelete(id);
+        return result;
+    }
+}
+
